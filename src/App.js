577d1a9fcd7aeaa32d50dc0dba191f88d75c82de
ryfoa6 <ryanmfoley84@gmail.com>
2021-07-28 15:28:19 -0700

    Build Header and Grid Container

--- src/App.js
+++ src/App.js
@@ -1,5 +1,38 @@
+import { Grid } from '@material-ui/core'
+import { makeStyles } from '@material-ui/core/styles'
+
+import Header from './components/Header/Header'
+
+const useStyles = makeStyles((theme) => ({
+	root: {
+		border: '2px solid pink',
+		background: 'pink',
+	},
+	container: {
+		border: '2px solid black',
+	},
+	header: {
+		width: '100%',
+		border: '2px solid red',
+	},
+	content: {
+		border: '2px solid blue',
+	},
+}))
+
 const App = () => {
-	return <div className='App'></div>
+	const classes = useStyles()
+
+	return (
+		<div className={classes.root}>
+			<Grid container direction='column'>
+				<Grid item>
+					<Header />
+				</Grid>
+				<Grid item container></Grid>
+			</Grid>
+		</div>
+	)
 }
 
 export default App
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,5 +1,60 @@
+import {
+	AppBar,
+	Button,
+	IconButton,
+	Toolbar,
+	Typography,
+} from '@material-ui/core'
+import { makeStyles } from '@material-ui/core/styles'
+import MenuIcon from '@material-ui/icons/Menu'
+
+import pokerIcon from '../../assets/images/poker-chips.png'
+
+const useStyles = makeStyles((theme) => ({
+	root: {
+		flexGrow: 1,
+		backgroundColor: '#333333',
+	},
+	menuButton: {
+		marginRight: theme.spacing(2),
+	},
+	title: {
+		flexGrow: 1,
+		margin: '0 15px',
+		fontFamily: 'Comfortaa, cursive',
+		fontWeight: 700,
+	},
+	loginButton: {
+		background: '#990100',
+	},
+}))
+
 const Header = () => {
-	return <div></div>
+	const classes = useStyles()
+
+	return (
+		<AppBar position='static' className={classes.root}>
+			<Toolbar>
+				<IconButton
+					edge='start'
+					className={classes.menuButton}
+					color='inherit'
+					aria-label='menu'>
+					<MenuIcon />
+				</IconButton>
+				<img src={pokerIcon} alt='poker chips icon' style={{ width: '50px' }} />
+				<Typography variant='h6' className={classes.title}>
+					HU HOLD'EM
+				</Typography>
+				<Button color='inherit' variant='outlined'>
+					Login
+				</Button>
+				<Button color='primary' variant='contained'>
+					Register
+				</Button>
+			</Toolbar>
+		</AppBar>
+	)
 }
 
 export default Header
