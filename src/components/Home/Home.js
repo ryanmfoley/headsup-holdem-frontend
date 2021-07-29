b0e014a1876f581e376b499903d453adff4581a4
ryfoa6 <ryanmfoley84@gmail.com>
2021-07-29 08:07:26 -0700

    Build Home with poker banner

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,5 +1,25 @@
+import { makeStyles } from '@material-ui/core/styles'
+
+import pokerBanner from '../../assets/images/poker-banner.jpg'
+
+const useStyles = makeStyles((theme) => ({
+	pokerBanner: {
+		width: '100%',
+	},
+}))
+
 const Home = () => {
-	return <div></div>
+	const classes = useStyles()
+
+	return (
+		<div>
+			<img
+				className={classes.pokerBanner}
+				src={pokerBanner}
+				alt='poker banner'
+			/>
+		</div>
+	)
 }
 
 export default Home
