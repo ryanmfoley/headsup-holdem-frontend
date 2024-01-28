373d8556c47bb9025feee52eb54f628726694f67
ryanmfoley <ryanmfoley84@gmail.com>
2024-01-27 16:32:25 -0800

    Update backend endpoint

--- src/config/config.js
+++ src/config/config.js
@@ -1,6 +1,6 @@
 const apiUrl =
 	window.location.hostname === 'localhost'
 		? 'http://localhost:3000'
-		: 'https://lucky-kringle-59480a.netlify.app'
+		: 'https://headsup-holdem-backend.onrender.com'
 
 export default apiUrl
